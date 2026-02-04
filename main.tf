b145c2275f7780bb80b29979e1c6bda12195be72
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-04 19:55:05 +0200

    Update backend configuration to use Azure for state management

--- main.tf
+++ main.tf
@@ -6,12 +6,16 @@ terraform {
     }
   }
 
-  backend "local" {
-    path     = "./terraform.tfstate"
-    use_oidc = true
+  backend "azurerm" {
+    storage_account_name = "whatislavxtfstate"
+    container_name       = "tfstate"
+    resource_group_name  = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
